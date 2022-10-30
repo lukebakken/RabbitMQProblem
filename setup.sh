eaf6214bc8ba5a8b1d7020e93d1291b434e409fd
Luke Bakken <luke@bakken.io>
2022-10-30 08:09:39 -0700

    setup.sh starts RabbitMQ on Linux

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
 sed "s|@@PWD@@|$PWD|" rabbitmq.conf.in > rabbitmq.conf
+if [ ! -d rabbitmq_server-3.11.2 ]
+then
+    curl -LO https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.11.2/rabbitmq-server-generic-unix-3.11.2.tar.xz
+    tar xf rabbitmq-server-generic-unix-3.11.2.tar.xz
+fi
+RABBITMQ_ALLOW_INPUT=true RABBITMQ_CONFIG_FILE="$PWD/rabbitmq.conf" LOG=debug ./rabbitmq_server-3.11.2/sbin/rabbitmq-server
