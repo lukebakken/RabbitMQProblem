d77b0d3e9b412b7238052a42ff70f9a2da0438a6
Luke Bakken <luke@bakken.io>
2022-10-30 08:41:31 -0700

    Enable EXTERNAL

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ if [ ! -d rabbitmq_server-3.11.2 ]
 then
     curl -LO https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.11.2/rabbitmq-server-generic-unix-3.11.2.tar.xz
     tar xf rabbitmq-server-generic-unix-3.11.2.tar.xz
+    ./rabbitmq_server-3.11.2/sbin/rabbitmq-plugins enable rabbitmq_management rabbitmq_auth_mechanism_ssl
 fi
 RABBITMQ_ALLOW_INPUT=true RABBITMQ_CONFIG_FILE="$PWD/rabbitmq.conf" LOG=debug ./rabbitmq_server-3.11.2/sbin/rabbitmq-server
